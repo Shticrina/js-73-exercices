992d70aa66551c763313d66984a62ebefecb5ead
cristina <cristinadinca.cd@gmail.com>
2020-11-04 17:40:36 +0100

    ex 9.4 & 9.5

--- 09-misc/04-worst-ui-two/script.js
+++ 09-misc/04-worst-ui-two/script.js
@@ -12,84 +12,45 @@
 
 (function() {
 
-	var targetElem = document.getElementById("target");
-	var targetElemText = targetElem.innerHTML;
-	targetElemText = targetElemText.replace("+", "0");
+	function changeString(buttonElem) {
+        var id = buttonElem.getAttribute("id");
+        var oldValue = parseInt(buttonElem.innerHTML);
+        var part1 = document.getElementById("part-one");
+        var part2 = document.getElementById("part-two");
+        var part3 = document.getElementById("part-three");
+        var part4 = document.getElementById("part-four");
 
-	function replaceString(buttonText, part) {
-		var oldValue = parseInt(buttonText);
-		var partToBeReplaced = "";
-		let replacingWith = "";
+        buttonElem.innerHTML = (buttonElem != part1 && oldValue < 9 ? "0" : "") + (++oldValue);
+        document.getElementById("target").innerHTML = "0" + part1.innerHTML + part2.innerHTML + part3.innerHTML + part4.innerHTML;
 
-		if (part == "one") {
-			partToBeReplaced = targetElemText.slice(1, 4);
-		} else if (part == "two") {
-			partToBeReplaced = targetElemText.slice(4, 6);
-		} else if (part == "three") {
-			partToBeReplaced = targetElemText.slice(7, 9);
-		} else if (part == "four") {
-			partToBeReplaced = targetElemText.slice(10, 11);
-		}
-		console.log("partToBeReplaced: " + partToBeReplaced);
+        // var targetElemText = document.getElementById("target").innerHTML;
+        // var partToBeReplaced = "";
+        // var replacingWith = "";
 
-		buttonText = ++oldValue;
-		document.getElementById("part-" + part).innerHTML = buttonText;
-		// let replacingWith = "0" + buttonText;
+        /*if (id == "part-one") {
+            partToBeReplaced = targetElemText.slice(1, 4);
+        } else if (id == "part-two") {
+            partToBeReplaced = targetElemText.slice(4, 6);
+        } else if (id == "part-three") {
+            partToBeReplaced = targetElemText.slice(6, 8);
+        } else if (id == "part-four") {
+            partToBeReplaced = targetElemText.slice(8, 10);
+        }
 
-		replacingWith = (part != "one" && oldValue < 9 ? "0" : "") + buttonText;
-		console.log("replacingWith: " + replacingWith);
-		
-		targetElemText = targetElemText.replace(partToBeReplaced, replacingWith); // replace data
+        replacingWith = (id != "part-one" && oldValue <= 9 ? "0" : "") + (++oldValue);
+        document.getElementById(id).innerHTML = replacingWith; // ok
 
-		targetElem.innerHTML = targetElemText;
-		console.log("str: " + targetElem.innerHTML);
-		console.log("---------------------");
-	}
+        // Here it replaces all 07 in the string
+        targetElemText = targetElemText.replace(partToBeReplaced, replacingWith); // replace data
+        document.getElementById("target").innerHTML = targetElemText;*/
+    }
 
-    document.getElementById("part-one").addEventListener("click", function() {
-    	replaceString(this.innerHTML, "one");
-		/*var oldValue = parseInt(this.innerHTML);
+    var buttons = document.querySelectorAll("button");
 
-    	this.innerHTML = ++oldValue;
-    	partOne = "0" + this.innerHTML;
-
-		let res = targetElemText.replace(targetElemText.slice(0, 4), partOne); // replace data
-    	targetElem.innerHTML = res;*/
-	});
-
-	document.getElementById("part-two").addEventListener("click", function() {
-		replaceString(this.innerHTML, "two");
-		/*var oldValue = parseInt(this.innerHTML);
-
-    	this.innerHTML = (oldValue < 9 ? "0" : "") + (++oldValue);
-    	partTwo = this.innerHTML; 
-
-    	let res = targetElemText.replace(targetElemText.slice(5, 7), partTwo); // replace data
-    	targetElem.innerHTML = res;*/
-	});
-
-	document.getElementById("part-three").addEventListener("click", function() {
-		replaceString(this.innerHTML, "three");
-		/*var oldValue = parseInt(this.innerHTML);
-
-    	this.innerHTML = (oldValue < 9 ? "0" : "") + (++oldValue);
-    	partThree = this.innerHTML;
-
-    	console.log(targetElemText);
-
-    	let res = targetElemText.replace(targetElemText.slice(8, 10), partThree); // replace data
-    	targetElem.innerHTML = res;*/
-	});
-
-	document.getElementById("part-four").addEventListener("click", function() {
-		replaceString(this.innerHTML, "four");
-		/*var oldValue = parseInt(this.innerHTML);
-
-    	this.innerHTML = (oldValue < 9 ? "0" : "") + (++oldValue);
-    	partFour = parseInt(this.innerHTML);*/
-		// partFour.innerHTML += this.innerHTML;
-	});
-
-	// console.log("str: " + targetElem.innerHTML);
+    buttons.forEach(buttonElem => {
+        buttonElem.addEventListener("click", function() {
+            changeString(this);
+        });
+    });
 
 })();
--- 09-misc/05-worst-ui-three/script.js
+++ 09-misc/05-worst-ui-three/script.js
@@ -11,6 +11,126 @@
 
 (function() {
 
-    // 
+    var buttons = document.querySelectorAll("button");
+    var stop_part_one = stop_part_two = stop_part_three = stop_part_four = false;
+
+	function stopInput(buttonElement) {
+		let currentInput = buttonElement.previousElementSibling;
+		let btnId = buttonElement.getAttribute("id");
+		let currentInputId = currentInput.getAttribute("id");
+
+		if (currentInputId == "part-one") {
+			stop_part_one = true;
+			clearInterval(randomInput1);
+		} else if (currentInputId == "part-two") {
+			stop_part_two = true;
+			clearInterval(randomInput2);
+		} else if (currentInputId == "part-three") {
+			stop_part_three = true;
+			clearInterval(randomInput3);
+		} else if (currentInputId == "part-four") {
+			stop_part_four = true;
+			clearInterval(randomInput4);
+		}
+
+		// clearInterval(randomInput);
+	}
+
+// *****************************************************************************************
+
+	function generateRandomNumber1() {
+		let inputElem = document.getElementById("part-one");
+
+		if (!stop_part_one) {
+			var minValue = parseInt(inputElem.getAttribute("data-min"));
+			var maxValue = parseInt(inputElem.getAttribute("data-max"));
+
+			inputElem.value = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+		}
+
+		if (!stop_part_one || !stop_part_two || !stop_part_three || !stop_part_four) {
+			document.getElementById("target").innerHTML = "0" + document.getElementById("part-one").value + document.getElementById("part-two").value + document.getElementById("part-three").value + document.getElementById("part-four").value;
+		}
+    }
+
+    function generateRandomNumber2() {
+		let inputElem = document.getElementById("part-two");
+
+		if (!stop_part_two) {
+			var minValue = parseInt(inputElem.getAttribute("data-min"));
+			var maxValue = parseInt(inputElem.getAttribute("data-max"));
+			var randomValue = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+
+			inputElem.value = (randomValue <= 9 ? "0" : "") + randomValue;
+		}
+
+		if (!stop_part_one || !stop_part_two || !stop_part_three || !stop_part_four) {
+			document.getElementById("target").innerHTML = "0" + document.getElementById("part-one").value + document.getElementById("part-two").value + document.getElementById("part-three").value + document.getElementById("part-four").value;
+		}
+    }
+
+    function generateRandomNumber3() {
+		let inputElem = document.getElementById("part-three");
+
+		if (!stop_part_three) {
+			var minValue = parseInt(inputElem.getAttribute("data-min"));
+			var maxValue = parseInt(inputElem.getAttribute("data-max"));
+			var randomValue = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+
+			inputElem.value = (randomValue <= 9 ? "0" : "") + randomValue;
+		}
+
+		if (!stop_part_one || !stop_part_two || !stop_part_three || !stop_part_four) {
+			document.getElementById("target").innerHTML = "0" + document.getElementById("part-one").value + document.getElementById("part-two").value + document.getElementById("part-three").value + document.getElementById("part-four").value;
+		}
+    }
+
+    function generateRandomNumber4() {
+		let inputElem = document.getElementById("part-four");
+
+		if (!stop_part_four) {
+			var minValue = parseInt(inputElem.getAttribute("data-min"));
+			var maxValue = parseInt(inputElem.getAttribute("data-max"));
+			var randomValue = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+
+			inputElem.value = (randomValue <= 9 ? "0" : "") + randomValue;
+		}
+
+		if (!stop_part_one || !stop_part_two || !stop_part_three || !stop_part_four) {
+			document.getElementById("target").innerHTML = "0" + document.getElementById("part-one").value + document.getElementById("part-two").value + document.getElementById("part-three").value + document.getElementById("part-four").value;
+		}
+    }
+
+// *****************************************************************************************
+
+	/*function generateRandomNumbers() {
+		document.querySelectorAll("input").forEach(inputElem => {
+			if (inputElem.getAttribute("id") == "part-one" && !stop_part_one ||
+				inputElem.getAttribute("id") == "part-two" && !stop_part_two ||
+				inputElem.getAttribute("id") == "part-three" && !stop_part_three ||
+				inputElem.getAttribute("id") == "part-four" && !stop_part_four) {
+				var minValue = parseInt(inputElem.getAttribute("data-min"));
+				var maxValue = parseInt(inputElem.getAttribute("data-max"));
+
+				inputElem.value = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+			}
+		});
+
+		if (!stop_part_one || !stop_part_two || !stop_part_three || !stop_part_four) {
+			document.getElementById("target").innerHTML = "0" + document.getElementById("part-one").value + document.getElementById("part-two").value + document.getElementById("part-three").value + document.getElementById("part-four").value;
+		}
+    }
+
+    var randomInput = setInterval(generateRandomNumbers, 30);*/
+    var randomInput1 = setInterval(generateRandomNumber1, 30);
+    var randomInput2 = setInterval(generateRandomNumber2, 30);
+    var randomInput3 = setInterval(generateRandomNumber3, 30);
+    var randomInput4 = setInterval(generateRandomNumber4, 30);
+
+    buttons.forEach(buttonElem => {
+        buttonElem.addEventListener("click", function() {
+            stopInput(this);
+        });
+    });
 
 })();
